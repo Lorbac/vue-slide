1de78c23f79bad267e241287dc2a2eea03977f0d
Lorenzo <baccini.lorenzo@gmail.com>
2021-09-08 16:17:19 +0200

    Fine bonus

--- js/main.js
+++ js/main.js
@@ -11,14 +11,24 @@ const app = new Vue ({
             "img/image4.jpg",
         ]
     },
+
+    created() {
+
+        // alert("ciao");
+        setInterval(() => {
+            this.counterPhoto += 1;
+            if (this.counterPhoto == (this.photos.length)) this.counterPhoto = 0;
+        }, 3000);
+    },
+
     methods: {
         prevPhoto() {
             this.counterPhoto -= 1;
-            if (this.counterPhoto < 0) this.counterPhoto = (this.photos.length-1);
+            if (this.counterPhoto < 0) this.counterPhoto = (this.photos.length - 1);
         },
         nextPhoto() {
             this.counterPhoto += 1;
-            if (this.counterPhoto == (this.photos.length)) this.counterPhoto = 0
+            if (this.counterPhoto == (this.photos.length)) this.counterPhoto = 0;
         }
     }
 });
